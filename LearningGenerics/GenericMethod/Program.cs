ecc24069a23f7cd03057109af970ce9f0f5f2603
ChenQiao <twins-cq@163.com>
2018-08-13 23:07:07 +0800

    泛型方法的简单使用

---
+++ LearningGenerics/GenericMethod/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"10+20={add(10, 20)}");
+            Console.WriteLine($"10.5+20.9={add(10.5, 20.9)}");
+            Console.WriteLine($"10+20.9={add(10, 20.9)}");
+            Console.WriteLine(add("hello", " world"));
+            Console.WriteLine("-----------------");
+            Console.WriteLine(Sum(100));
+            Console.ReadLine();
+        }
+        //泛型方法简单使用
+        static T add<T>(T a, T b)
+        {
+            dynamic a1 = a;
+            dynamic b1 = b;
+            return a1 + b1;
+        }
+        //泛型方法实现一个数求和
+        static T Sum<T>(T a) where T : struct
+        {
+            dynamic sum = default(T);
+            for (dynamic i = default(T); i <= a; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
