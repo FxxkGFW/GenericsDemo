4b7e05071016bdc4c9b2149f21b1ea66d4b07964
ChenQiao <twins-cq@163.com>
2018-08-13 16:57:53 +0800

    泛型类的基本使用

---
+++ LearningGenerics/LearningGenerics/MyStack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningGenerics
+{
+    /// <summary>
+    /// 使用泛型类实现出入栈通用类
+    /// </summary>
+    public class MyStack<T>
+    {
+        T[] stack;
+        int size;//栈数据容量
+        int stackPoint;//栈位置指针
+
+        public MyStack(int size)
+        {
+            this.size = size;
+            stack = new T[size];
+            stackPoint = -1;
+        }
+        /// <summary>
+        /// 入栈
+        /// </summary>
+        /// <param name="item"></param>
+        public void Push(T item)
+        {
+            stackPoint++;
+            if (stackPoint >= size)
+            {
+                Console.WriteLine("栈空间已满");
+                return;
+            }
+            stack[stackPoint] = item;
+        }
+        /// <summary>
+        /// 出栈
+        /// </summary>
+        /// <returns></returns>
+        public T Pop()
+        {
+            stackPoint--;
+            if (stackPoint < 0)
+            {
+                Console.WriteLine("栈空间已空");
+                return default(T);
+            }
+            T data = stack[stackPoint];
+            return data;
+        }
+    }
+}
--- LearningGenerics/LearningGenerics/Program.cs
+++ LearningGenerics/LearningGenerics/Program.cs
@@ -10,6 +10,33 @@ namespace LearningGenerics
     {
         static void Main(string[] args)
         {
+            MyStack<int> stack1 = new MyStack<int>(5);
+            stack1.Push(1);
+            stack1.Push(2);
+            stack1.Push(3);
+            stack1.Push(4);
+            stack1.Push(5);
+            stack1.Push(6);
+            Console.WriteLine(stack1.Pop());
+            Console.WriteLine(stack1.Pop());
+            Console.WriteLine(stack1.Pop());
+            Console.WriteLine(stack1.Pop());
+            Console.WriteLine(stack1.Pop());
+            Console.WriteLine(stack1.Pop());
+            MyStack<string> stack2 = new MyStack<string>(5);
+            stack2.Push("产品1");
+            stack2.Push("产品2");
+            stack2.Push("产品3");
+            stack2.Push("产品4");
+            stack2.Push("产品5");
+            stack2.Push("产品6");
+            Console.WriteLine(stack2.Pop());
+            Console.WriteLine(stack2.Pop());
+            Console.WriteLine(stack2.Pop());
+            Console.WriteLine(stack2.Pop());
+            Console.WriteLine(stack2.Pop());
+            Console.WriteLine(stack2.Pop());
+            Console.Read();
         }
     }
 }
